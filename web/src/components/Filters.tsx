b03aac5160d613b2a266d55d16ca6dccee5c63ba
Zonian <57513632+ZonianMidian@users.noreply.github.com>
2022-11-04 13:44:12 -0500

    Correción Menor [Traducción]

--- web/src/components/Filters.tsx
+++ web/src/components/Filters.tsx
@@ -15,11 +15,11 @@ const FiltersContainer = styled.form`
     background: var(--bg-bright);
     border-bottom-left-radius: 3px;
     border-bottom-right-radius: 3px;
-    margin: 0 auto;
+	margin: 0 auto;
     z-index: 99;
 
     > * {
-        margin-right: 15px !important;
+        margin-right: 15px !important;    
 
         &:last-child {
             margin-right: 0 !important;
@@ -45,61 +45,27 @@ export function Filters() {
             const channel = data.get("channel") as string | null;
             const username = data.get("username") as string | null;
 
-            queryClient.invalidateQueries([
-                "log",
-                {
-                    channel: channel?.toLowerCase(),
-                    username: username?.toLowerCase(),
-                },
-            ]);
+            queryClient.invalidateQueries(["log", { channel: channel?.toLowerCase(), username: username?.toLowerCase() }]);
 
             setCurrents(channel, username);
         }
     };
 
-    return (
-        <FiltersWrapper>
-            <FiltersContainer onSubmit={handleSubmit} action="none">
-                <Autocomplete
-                    id="autocomplete-channels"
-                    options={channels.map((channel) => channel.name)}
-                    style={{ width: 225 }}
-                    defaultValue={state.currentChannel}
-                    getOptionLabel={(channel: string) => channel}
-                    clearOnBlur={false}
-                    renderInput={(params) => (
-                        <TextField
-                            {...params}
-                            name="channel"
-                            label="Canal o id:123"
-                            variant="filled"
-                            autoFocus={state.currentChannel === null}
-                        />
-                    )}
-                />
-                <TextField
-                    error={state.error}
-                    name="username"
-                    label="Usuario o id:123"
-                    variant="filled"
-                    autoComplete="off"
-                    defaultValue={state.currentUsername}
-                    autoFocus={
-                        state.currentChannel !== null &&
-                        state.currentUsername === null
-                    }
-                />
-                <Button
-                    variant="contained"
-                    color="primary"
-                    size="large"
-                    type="submit"
-                >
-                    load
-                </Button>
-                <Settings />
-                <Docs />
-            </FiltersContainer>
-        </FiltersWrapper>
-    );
+    return <FiltersWrapper>
+        <FiltersContainer onSubmit={handleSubmit} action="none">
+            <Autocomplete
+                id="autocomplete-channels"
+                options={channels.map(channel => channel.name)}
+                style={{ width: 225 }}
+                defaultValue={state.currentChannel}
+                getOptionLabel={(channel: string) => channel}
+                clearOnBlur={false}
+                renderInput={(params) => <TextField {...params} name="channel" label="Canal o id:123" variant="filled" autoFocus={state.currentChannel === null} />}
+            />
+            <TextField error={state.error} name="username" label="Usuario o id:123" variant="filled" autoComplete="off" defaultValue={state.currentUsername} autoFocus={state.currentChannel !== null && state.currentUsername === null} />
+            <Button variant="contained" color="primary" size="large" type="submit">cargar</Button>
+            <Settings />
+            <Docs />
+        </FiltersContainer>
+    </FiltersWrapper>
 }
